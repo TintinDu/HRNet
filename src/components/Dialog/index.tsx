2f12efe4ecfcd9a2bbd9c6d02672d9fb5704501f
martin.dubois <martin.dubois@foncia.com>
2024-07-12 13:27:54 +0200

    feat: add default style to dialog

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -7,7 +7,27 @@ interface DialogProps {
 
 export const Dialog = ({ isOpen, setIsOpen }: DialogProps) => {
   return (
-    <TintinDuDialog isOpen={isOpen} onClose={() => setIsOpen(false)}>
+    <TintinDuDialog
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      closeButtonStyle="icon"
+      customStyles={{
+        content: {
+          width: "50%",
+          height: "50%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+          borderRadius: 10,
+          padding: 20,
+        },
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        },
+      }}
+    >
       <h1>Employee Created</h1>
       <p>Your form has been submitted successfully</p>
     </TintinDuDialog>
